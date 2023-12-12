2668c37a25da2bda971c95efa1c982ec62d6bf32
lggg123 <machinelearner2334@gmail.com>
2023-12-12 13:25:16 -0800

    changes

--- scripts/mint.js
+++ scripts/mint.js
@@ -5,7 +5,7 @@ async function main() {
     const recieverAddress = "0xEA3AF2F7fC9FD23DAEa15D8d5E38B510E6830780"
     const batchNFTs = await hre.ethers.getContractAt("BatchNFTs", contractAddress);
 
-    const mintTokens = await batchNFTs.mint(recieverAddress, 2657, { value: "2657000000000000000" }); // 0.03 ETH in wei
+    const mintTokens = await batchNFTs.mint(recieverAddress, 2657, { value: "26570000000000000000" }); // 0.03 ETH in wei
     console.log(`Transaction Hash: https://mumbai.polygonscan.com/tx/${mintTokens.hash}`);
 }
 
